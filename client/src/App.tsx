56ecadaee0461136d729703591f46b3a1545c605
Frank-sys486 <leofranklinborromeo@gmail.com>
2025-12-21 21:17:30 +0800

    remove dashboard page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,6 @@ import { ThemeProvider } from "@/context/ThemeContext";
 import { Navbar } from "@/components/Navbar";
 import Home from "@/pages/Home";
 import Fleet from "@/pages/Fleet";
-import Dashboard from "@/pages/Dashboard";
 import NotFound from "@/pages/not-found";
 
 function Router() {
@@ -16,7 +15,6 @@ function Router() {
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/fleet" component={Fleet} />
-      <Route path="/dashboard" component={Dashboard} />
       <Route component={NotFound} />
     </Switch>
   );
